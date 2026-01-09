655e1e0fdea5232c8dc03abf8b5c29f121d4bc8f
Hector <joaohectorfraga@hotmail.com>
2026-01-09 20:23:59 -0300

    created 1003.c

---
+++ iniciante/1003/1003.c
@@ -0,0 +1,14 @@
+// 1003 - Soma Simples - beecrowd
+
+#include <stdio.h>
+
+int main(void)
+{
+    int A, B; scanf("%d %d", &A, &B);
+    int SOMA;
+    
+    SOMA = A + B;
+    printf("SOMA = %d\n", SOMA);
+    
+    return 0;
+}
