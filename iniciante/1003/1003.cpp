5407480770f346bf74f01e12f3775518e0de5e4a
Hector <joaohectorfraga@hotmail.com>
2026-01-09 20:24:32 -0300

    created 1003.cpp

---
+++ iniciante/1003/1003.cpp
@@ -0,0 +1,15 @@
+// 1003 - Soma Simples - beecrowd
+
+#include <iostream>
+
+int main(void)
+{
+    int A, B; std::cin >> A >> B;
+    int SOMA;
+
+    SOMA = A + B;
+
+    std::cout << "SOMA = " << SOMA << '\n';
+
+    return 0;
+}
