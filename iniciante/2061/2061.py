649ab6d69a394afd176fddbebf2dd43bd18b8742
Hector <joaohectorfraga@hotmail.com>
2026-01-11 14:39:12 -0300

    created 2061.py

---
+++ iniciante/2061/2061.py
@@ -0,0 +1,16 @@
+# 2061 - As Abas de Pericles - beecrowd
+
+def calc(a : int, x : int):
+    for i in range(x):
+        c = str(input())
+        if c == "fechou": a += 1
+        if c == "clicou": a -= 1
+    
+    print(0 if a < 0 else a)
+
+def main():
+    N, M = map(int, input().split())
+    calc(N, M)
+
+if __name__ == "__main__":
+    main()
