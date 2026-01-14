6ef77f56ba56f2bf462c8d42e65c43b4818e4b47
Hector <joaohectorfraga@hotmail.com>
2026-01-14 20:36:50 -0300

    changes to code 1985.c

--- iniciante/1985/1985.c
+++ iniciante/1985/1985.c
@@ -2,35 +2,19 @@
 
 #include <stdio.h>
 
-double calc(int cod, int qtd)
+int main(void)
 {
-    double precos[] = {1.5, 2.5, 3.5, 4.5, 5.5};
-    int codigos[] = {1001, 1002, 1003, 1004, 1005};
-    double tp = 0; // total pedido
-    int i; // index
+    double precos[5] = {1.5, 2.5, 3.5, 4.5, 5.5};
+    int p, c, q;
+    double tc = 0;
 
-    for (i = 0; i < sizeof(codigos) / sizeof(codigos[0]); i++) {
-        if (cod == codigos[i]) tp += precos[i] * qtd;
-    }
+    scanf("%d", &p);
 
-    return tp;
-}
-
-int main(void) {
-    double tc = 0; // total compra
-    int i; // index
-    int c, q; // codigo, quantidade
-
-    // pedidos
-    int p; scanf("%d", &p);
-
-
-    for (i = 0; i < p; i++) {
+    for (int i = 0; i < p; i++) {
         scanf("%d %d", &c, &q);
-        tc += calc(c, q);
+        tc += precos[c - 1001] * q;
     }
 
     printf("%.2lf\n", tc);
-
     return 0;
 }
