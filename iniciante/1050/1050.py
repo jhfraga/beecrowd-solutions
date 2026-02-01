ad0cccc5cf04af01c5d1bd12ef052e24c2455e08
Hector <joaohectorfraga@hotmail.com>
2026-02-01 17:26:18 -0300

    created 1050.py

---
+++ iniciante/1050/1050.py
@@ -0,0 +1,16 @@
+# 1050 - DDD - beecrowd
+
+destination = {
+    61 : "Brasilia",
+    71 : "Salvador",
+    11 : "Sao Paulo",
+    21 : "Rio de Janeiro",
+    32 : "Juiz de Fora",
+    19 : "Campinas",
+    27 : "Vitoria",
+    31 : "Belo Horizonte"
+}
+
+ddd = int(input())
+
+print("DDD nao cadastrado" if not ddd in destination else destination[ddd])
