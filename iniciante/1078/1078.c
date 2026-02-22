5effe37ff8dfec554acbfd8e1c94c2ada4f73aab
Hector <joaohectorfraga@hotmail.com>
2026-02-22 12:56:12 -0300

    created 1078.c

---
+++ iniciante/1078/1078.c
@@ -0,0 +1,15 @@
+// 1078 - Tabuada - beecrowd
+
+#include <stdio.h>
+
+int main(void)
+{
+    int N; scanf("%d", &N);
+
+    for (int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", i, N, (i * N));
+    }
+
+    return 0;
+}
+
