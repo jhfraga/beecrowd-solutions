c0079f3775f5796a97f18373fe87102b01fda937
Hector <joaohectorfraga@hotmail.com>
2026-04-18 17:25:30 -0300

    modified 1095.c

---
+++ iniciante/1095/1095.c
@@ -0,0 +1,17 @@
+// 1095 - Sequencia IJ 1 - beecrowd
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 1, j = 60;
+
+	while (j >= 0)
+	{
+		printf("I=%d J=%d\n", i, j);
+		j-=5; i+=3;
+	}
+
+	return 0;
+}
+
