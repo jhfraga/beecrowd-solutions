559d384209bfeb194643baa6c0075903863538e3
Hector <joaohectorfraga@hotmail.com>
2026-04-20 19:01:16 -0300

    created 3055.cpp

---
+++ iniciante/3055/3055.cpp
@@ -0,0 +1,18 @@
+// 3055 - Nota esquecida - beecrowd
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int A, B,M;
+	cin >> A >> M;
+
+	B = M * 2 -A;
+
+	cout << B << "\n";	
+
+	return 0;
+}
+
