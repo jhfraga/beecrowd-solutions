a1552c43918504e5fcce9bc7f95fa838885351f3
Hector <joaohectorfraga@hotmail.com>
2026-05-09 14:00:26 -0300

    created 1007.js

---
+++ iniciante/1007/1007.js
@@ -0,0 +1,14 @@
+// 1007 - Diferenca - beecrowd
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const A = parseInt(lines[0]);
+const B = parseInt(lines[1]);
+const C = parseInt(lines[2]);
+const D = parseInt(lines[3]);
+
+const DIF = (A * B - C * D);
+
+console.log(`DIFERENCA = ${DIF}`);
+
