9f2106c07994473d1e130f3492ee1aa798a10fba
Hector <joaohectorfraga@hotmail.com>
2026-05-08 21:52:24 -0300

    created 1003.js

---
+++ iniciante/1003/1003.js
@@ -0,0 +1,12 @@
+// 1003 - Soma Simples - beecrowd
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const A = parseInt(lines[0]);
+const B = parseInt(lines[1]);
+
+const SOMA = A + B;
+
+console.log('SOMA = ' + SOMA);
+
